26e582348669a5df0f6f241c6911b24a68c0ffcf
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2023-10-18 13:34:54 +0530

    updated tnc and policy page

--- pages/termsofuse.js
+++ pages/termsofuse.js
@@ -18,14 +18,14 @@ const termsofuse = () => {
         </h1>
         <div className="space-y-2">
           <h2 className="text-black text-lg font-semibold">
-            AGREEMENT BETWEEN USER AND TENSORACT INC
+            AGREEMENT BETWEEN USER AND Karini AI LLM
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            The Tensoract Inc Web Site is comprised of various Web pages
-            operated by Tensoract Inc. The Tensoract Inc Web Site is offered to
+            The Karini AI LLM Web Site is comprised of various Web pages
+            operated by Karini AI LLM. The Karini AI LLM Web Site is offered to
             you conditioned on your acceptance without modification of the
             terms. conditions and notices contained herein. Your use of the
-            Tensoract Inc Web Site constitutes your agreement to all such terms.
+            Karini AI LLM Web Site constitutes your agreement to all such terms.
             conditions. and notices.
           </p>
         </div>
@@ -34,10 +34,10 @@ const termsofuse = () => {
             MODIFICATION OF THESE TERMS OF USE
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            Tensoract Inc reserves the right to change the terms & conditions
-            and notices under which the Tensoract Inc Web Site is offered.
+            Karini AI LLM reserves the right to change the terms & conditions
+            and notices under which the Karini AI LLM Web Site is offered.
             including but not limited to the charges associated with the use of
-            the Tensoract Inc Web Site.
+            the Karini AI LLM Web Site.
           </p>
         </div>
         <div className="space-y-2">
@@ -45,15 +45,15 @@ const termsofuse = () => {
             LINKS TO THIRD PARTY SITES
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            The Tensoract Inc Web Site may contain links to other Web Sites
+            The Karini AI LLM Web Site may contain links to other Web Sites
             (&quot;Linked Sites&quot;). The Linked Sites are not under the
-            control of Tensoract Inc and Tensoract Inc is not responsible for
+            control of Karini AI LLM and Karini AI LLM is not responsible for
             the contents of any Linked Site. including without limitation any
             link contained in a Linked Site. or any changes or updates to a
-            Linked Site. Tensoract Inc is not responsible for webcasting or any
+            Linked Site. Karini AI LLM is not responsible for webcasting or any
             other form of transmission received from any Linked Site. Tensoract
             Inc is providing these links to you only as a convenience. and the
-            inclusion of any link does not imply endorsement by Tensoract Inc of
+            inclusion of any link does not imply endorsement by Karini AI LLM of
             the site or any association with its operators.
           </p>
         </div>
@@ -62,13 +62,13 @@ const termsofuse = () => {
             NO UNLAWFUL OR PROHIBITED USE
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            As a condition of your use of the Tensoract Inc Web Site, you
-            warrant to Tensoract Inc that you will not use the Tensoract Inc Web
+            As a condition of your use of the Karini AI LLM Web Site, you
+            warrant to Karini AI LLM that you will not use the Karini AI LLM Web
             Site for any purpose that is unlawful or prohibited by these terms,
-            conditions. and notices. You may not use the Tensoract Inc Web Site
+            conditions. and notices. You may not use the Karini AI LLM Web Site
             in any manner which could damage. disable. overburden. or impair the
-            Tensoract Inc Web Site or interfere with any other party&apos;s use
-            and enjoyment of the Tensoract Inc Web Site. You may not obtain or
+            Karini AI LLM Web Site or interfere with any other party&apos;s use
+            and enjoyment of the Karini AI LLM Web Site. You may not obtain or
             attempt to obtain any materials or information through any means not
             intentionally made available or provided for through the Tensoract
             Inc Web Sites.
@@ -79,7 +79,7 @@ const termsofuse = () => {
             USE OF COMMUNICATION SERVICES
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            The Tensoract Inc Web Site may contain bulletin board services. chat
+            The Karini AI LLM Web Site may contain bulletin board services. chat
             areas. news groups. forums. communities. personal web pages.
             calendars. and/or other message or communication facilities designed
             to enable you to communicate with the public at large or with a
@@ -140,13 +140,13 @@ const termsofuse = () => {
               applicable for any particular Communication Service.
             </p>
             <p className="text-secondary-700 text-sm leading-relaxed">
-              - Tensoract Inc has no obligation to monitor the Communication
-              Services. However. Tensoract Inc reserves the right to review
+              - Karini AI LLM has no obligation to monitor the Communication
+              Services. However. Karini AI LLM reserves the right to review
               materials posted to a Communication Service and to remove any
               materials in its sole discretion.
             </p>
             <p className="text-secondary-700 text-sm leading-relaxed">
-              - Tensoract Inc reserves the right to terminate your access to any
+              - Karini AI LLM reserves the right to terminate your access to any
               or all of the Communication Services at any time without notice
               for any reason whatsoever.
             </p>
@@ -155,7 +155,7 @@ const termsofuse = () => {
               information as necessary to satisfy any applicable law.
               regulation. legal process or governmental request. or to edit.
               refuse to post or to remove any information or materials. in whole
-              or in part. in Tensoract Inc sole discretion.
+              or in part. in Karini AI LLM sole discretion.
             </p>
             <p className="text-secondary-700 text-sm leading-relaxed">
               - Always use caution when giving out any personally identifying
@@ -163,13 +163,13 @@ const termsofuse = () => {
               Service.
             </p>
             <p className="text-secondary-700 text-sm leading-relaxed">
-              - Tensoract Inc does not control or endorse the content. messages
+              - Karini AI LLM does not control or endorse the content. messages
               or information found in any Communication Service and. therefore.
-              Tensoract Inc specifically disclaims any liability with regard to
+              Karini AI LLM specifically disclaims any liability with regard to
               the Communication Services and any actions resulting from your
               participation in any Communication Service. Managers and hosts are
-              not authorized Tensoract Inc spokespersons. and their views do not
-              necessarily reflect those of Tensoract Inc.
+              not authorized Karini AI LLM spokespersons. and their views do not
+              necessarily reflect those of Karini AI LLM.
             </p>
             <p className="text-secondary-700 text-sm leading-relaxed">
               - Materials uploaded to a Communication Service may be subject to
@@ -181,16 +181,16 @@ const termsofuse = () => {
         </div>
         <div className="space-y-2">
           <h2 className="text-black text-lg font-semibold">
-            MATERIALS PROVIDED TO TENSORACT INC OR POSTED AT ANY TENSORACT INC
+            MATERIALS PROVIDED TO Karini AI LLM OR POSTED AT ANY Karini AI LLM
             WEBSITE
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            Tensoract Inc does not claim ownership of the materials you provide
-            to Tensoract Inc (including feedback and suggestions) or post.
-            upload. input or submit to any Tensoract Inc Web Site or its
+            Karini AI LLM does not claim ownership of the materials you provide
+            to Karini AI LLM (including feedback and suggestions) or post.
+            upload. input or submit to any Karini AI LLM Web Site or its
             associated services (collectively &apos;Submissions&apos;). However.
             by posting. uploading. inputting. providing or submitting your
-            Submission you are granting Tensoract Inc. its affiliated companies
+            Submission you are granting Karini AI LLM. its affiliated companies
             and necessary sublicensees permission to use your Submission in
             connection with the operation of their Internet businesses
             including. without limitation. the rights to: copy. distribute.
@@ -199,7 +199,7 @@ const termsofuse = () => {
             connection with your Submission. No compensation will be paid with
             respect to the use of your Submission. as provided herein. Tensoract
             Inc is under no obligation to post or use any Submission you may
-            provide and may remove any Submission at any time in Tensoract Inc
+            provide and may remove any Submission at any time in Karini AI LLM
             sole discretion. By posting. uploading. inputting. providing or
             submitting your Submission you warrant and represent that you own or
             otherwise control all of the rights to your Submission as described
@@ -214,45 +214,45 @@ const termsofuse = () => {
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
             THE INFORMATION. SOFTWARE. PRODUCTS. AND SERVICES INCLUDED IN OR
-            AVAILABLE THROUGH THE TENSORACT INC WEB SITE MAY INCLUDE
+            AVAILABLE THROUGH THE Karini AI LLM WEB SITE MAY INCLUDE
             INACCURACIES OR TYPOGRAPHICAL ERRORS. CHANGES ARE PERIODICALLY ADDED
-            TO THE INFORMATION HEREIN. TENSORACT INC AND/OR ITS SUPPLIERS MAY
-            MAKE IMPROVEMENTS AND/OR CHANGES IN THE TENSORACT INC WEB SITE AT
-            ANY TIME. ADVICE RECEIVED VIA THE TENSORACT INC WEB SITE SHOULD NOT
+            TO THE INFORMATION HEREIN. Karini AI LLM AND/OR ITS SUPPLIERS MAY
+            MAKE IMPROVEMENTS AND/OR CHANGES IN THE Karini AI LLM WEB SITE AT
+            ANY TIME. ADVICE RECEIVED VIA THE Karini AI LLM WEB SITE SHOULD NOT
             BE RELIED UPON FOR PERSONAL. MEDICAL. LEGAL OR FINANCIAL DECISIONS
             AND YOU SHOULD CONSULT AN APPROPRIATE PROFESSIONAL FOR SPECIFIC
-            ADVICE TAILORED TO YOUR SITUATION. TENSORACT INC AND/OR ITS
+            ADVICE TAILORED TO YOUR SITUATION. Karini AI LLM AND/OR ITS
             SUPPLIERS MAKE NO REPRESENTATIONS ABOUT THE SUITABILITY.
             RELIABILITY. AVAILABILITY. TIMELINESS. AND ACCURACY OF THE
             INFORMATION, SOFTWARE, PRODUCTS. SERVICES AND RELATED GRAPHICS
-            CONTAINED ON THE TENSORACT INC WEB SITE FOR ANY PURPOSE. TO THE
+            CONTAINED ON THE Karini AI LLM WEB SITE FOR ANY PURPOSE. TO THE
             MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW. ALL SUCH INFORMATION,
             SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS ARE PROVIDED AS IS
-            WITHOUT WARRANTY OR CONDITION OF ANY KIND. Tensoract Inc AND/OR ITS
+            WITHOUT WARRANTY OR CONDITION OF ANY KIND. Karini AI LLM AND/OR ITS
             SUPPLIERS HEREBY DISCLAIM ALL WARRANTIES AND CONDITIONS WITH REGARD
             TO THIS INFORMATION. SOFTWARE. PRODUCTS. SERVICES AND RELATED
             GRAPHICS. INCLUDING ALL IMPLIED WARRANTIES OR CONDITIONS OF
             MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
             NON-INFRINGEMENT. TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW.
-            IN NO EVENT SHALL TENSORACT INC AND/OR ITS SUPPLIERS BE LIABLE FOR
+            IN NO EVENT SHALL Karini AI LLM AND/OR ITS SUPPLIERS BE LIABLE FOR
             ANY DIRECT. INDIRECT. PUNITIVE. INCIDENTAL. SPECIAL. CONSEQUENTIAL
             DAMAGES OR ANY DAMAGES WHATSOEVER INCLUDING. WITHOUT LIMITATION.
             DAMAGES FOR LOSS OF USE. DATA OR PROFITS. ARISING OUT OF OR IN ANY
-            WAY CONNECTED WITH THE USE OR PERFORMANCE OF THE TENSORACT INC WEB
-            SITE. WITH THE DELAY OR INABILITY TO USE THE TENSORACT INC WEB SITE
+            WAY CONNECTED WITH THE USE OR PERFORMANCE OF THE Karini AI LLM WEB
+            SITE. WITH THE DELAY OR INABILITY TO USE THE Karini AI LLM WEB SITE
             OR RELATED SERVICES. THE PROVISION OF OR FAILURE TO PROVIDE
             SERVICES. OR FOR ANY INFORMATION. SOFTWARE. PRODUCTS. SERVICES AND
-            RELATED GRAPHICS OBTAINED THROUGH THE TENSORACT INC WEB SITE. OR
-            OTHERWISE ARISING OUT OF THE USE OF THE TENSORACT INC WEB SITE.
+            RELATED GRAPHICS OBTAINED THROUGH THE Karini AI LLM WEB SITE. OR
+            OTHERWISE ARISING OUT OF THE USE OF THE Karini AI LLM WEB SITE.
             WHETHER BASED ON CONTRACT. TORT. NEGLIGENCE. STRICT LIABILITY OR
-            OTHERWISE. EVEN IF TENSORACT INC OR ANY OF ITS SUPPLIERS HAS BEEN
+            OTHERWISE. EVEN IF Karini AI LLM OR ANY OF ITS SUPPLIERS HAS BEEN
             ADVISED OF THE POSSIBILITY OF DAMAGES. BECAUSE SOME
             STATES/JURISDICTIONS DO NOT ALLOW THE EXCLUSION OR LIMITATION OF
             LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES. THE ABOVE
             LIMITATION MAY NOT APPLY TO YOU. IF YOU ARE DISSATISFIED WITH ANY
-            PORTION OF THE TENSORACT INC WEB SITE. OR WITH ANY OF THESE TERMS OF
+            PORTION OF THE Karini AI LLM WEB SITE. OR WITH ANY OF THESE TERMS OF
             USE. YOUR SOLE AND EXCLUSIVE REMEDY IS TO DISCONTINUE USING THE
-            TENSORACT INC WEB SITE
+            Karini AI LLM WEB SITE
           </p>
         </div>
         <div className="space-y-2">
@@ -260,25 +260,25 @@ const termsofuse = () => {
             TERMINATION/ACCESS RESTRICTION
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            Tensoract Inc reserves the right. in its sole discretion. to
-            terminate your access to the Tensoract Inc Web Site and the related
+            Karini AI LLM reserves the right. in its sole discretion. to
+            terminate your access to the Karini AI LLM Web Site and the related
             services or any portion thereof at any time. without notice. GENERAL
             To the maximum extent permitted by law. this agreement is governed
             by the laws of the State of Washington. U.S.A. and you hereby
             consent to the exclusive jurisdiction and venue of courts in San
             Mateo County. California. U.S.A. in all disputes arising out of or
-            relating to the use of the Tensoract Inc Web Site. Use of the
-            Tensoract Inc Web Site is unauthorized in any jurisdiction that does
-            not give effect to all provisions of these terms and conditions,
+            relating to the use of the Karini AI LLM Web Site. Use of the Karini
+            AI LLM Web Site is unauthorized in any jurisdiction that does not
+            give effect to all provisions of these terms and conditions,
             including without limitation this paragraph. You agree that no joint
             venture, partnership, employment, or agency relationship exists
-            between you and Tensoract Inc as a result of this agreement or use
-            of the Tensoract Inc Web Site. Tensoract Inc performance of this
+            between you and Karini AI LLM as a result of this agreement or use
+            of the Karini AI LLM Web Site. Karini AI LLM performance of this
             agreement is subject to existing laws and legal process. and nothing
-            contained in this agreement is in derogation of Tensoract Inc right
+            contained in this agreement is in derogation of Karini AI LLM right
             to comply with governmental. court and law enforcement requests or
-            requirements relating to your use of the Tensoract Inc Web Site or
-            information provided to or gathered by Tensoract Inc with respect to
+            requirements relating to your use of the Karini AI LLM Web Site or
+            information provided to or gathered by Karini AI LLM with respect to
             such use. If any part of this agreement is determined to be invalid
             or unenforceable pursuant to applicable law including, but not
             limited to, the warranty disclaimers and liability limitations set
@@ -287,11 +287,11 @@ const termsofuse = () => {
             closely matches the intent of the original provision and the
             remainder of the agreement shall continue in effect. Unless
             otherwise specified herein. this agreement constitutes the entire
-            agreement between the user and Tensoract Inc with respect to the
-            Tensoract Inc Web Site and it supersedes all prior or
+            agreement between the user and Karini AI LLM with respect to the
+            Karini AI LLM Web Site and it supersedes all prior or
             contemporaneous communications and proposals. whether electronic.
-            oral or written. between the user and Tensoract Inc with respect to
-            the Tensoract Inc Web Site. A printed version of this agreement and
+            oral or written. between the user and Karini AI LLM with respect to
+            the Karini AI LLM Web Site. A printed version of this agreement and
             of any notice given in electronic form shall be admissible in
             judicial or administrative proceedings based upon or relating to
             this agreement to the same extent an d subject to the same
@@ -306,7 +306,7 @@ const termsofuse = () => {
             COPYRIGHT AND TRADEMARK NOTICES
           </h2>
           <p className="text-secondary-700 text-sm leading-relaxed">
-            All contents of the Tensoract Inc Web Site are: 2023 by Tensoract
+            All contents of the Karini AI LLM Web Site are: 2023 by Tensoract
             Inc and/or its suppliers. All rights reserved.
           </p>
         </div>
