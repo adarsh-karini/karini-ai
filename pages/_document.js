2b1d4e1afa4d974cd60a99433dce6d756eafbfca
Venketesh-Rushi <venketesh.rushi@karini.ai>
2023-12-20 15:12:58 +0530

    added google tag manager script in _document file

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,39 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="icon" type="image/x-icon" href="/favicon.png" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<link rel="icon" type="image/x-icon" href="/favicon.png" />
+				{/* Google Tag Manager */}
+				<Script
+					id="gtm-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){
+									w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+									var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+									j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+									f.parentNode.insertBefore(j,f);
+								})(window,document,'script','dataLayer','GTM-KBTD3M2M');
+								`,
+					}}
+				></Script>
+				{/* End Google Tag Manager */}
+			</Head>
+			<body>
+				{/* Google Tag Manager (noscript) */}
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBTD3M2M" height="0" width="0" style="display: none; visibility: hidden;" />`,
+					}}
+				/>
+
+				{/* End Google Tag Manager (noscript) */}
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
