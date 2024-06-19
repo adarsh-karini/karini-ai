95771e70c9df5e7585ad73bda7b043ebcc4710e5
Swapnil Kotkar <swapnil.kotkar@karini.ai>
2024-06-19 20:51:49 +0530

    updated aws api

--- pages/api/aws_marketplace_registration.js
+++ pages/api/aws_marketplace_registration.js
@@ -2,9 +2,13 @@ export default function handler(req, res) {
 	if (req.method === "POST") {
 		const data = req.body;
 
+		console.log(`Received req: ${JSON.stringify(req, null, 2)}`);
+
 		console.log(`Received data: ${JSON.stringify(data, null, 2)}`);
 
-		let redirectUrl = `https://www.karini.ai/aws_marketplace_registration.html?x-amzn-marketplace-token=${data.registrationToken}`;
+		let token = data["x-amzn-marketplace-token"];
+
+		let redirectUrl = `https://www.karini.ai/aws_marketplace_registration.html?x-amzn-marketplace-token=${token}`;
 
 		res.redirect(redirectUrl);
 	} else {
