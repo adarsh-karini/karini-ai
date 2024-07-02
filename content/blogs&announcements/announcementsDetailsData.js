8781e4c10f0e25cf81fc0bfb0f269754fc8255e2
venketesh_rushi <venketesh.rushi@karini.ai>
2024-07-02 16:36:52 +0530

    new02

--- content/blogs&announcements/announcementsDetailsData.js
+++ content/blogs&announcements/announcementsDetailsData.js
@@ -1173,6 +1173,175 @@ export const announcementsDetailsData = [
 	},
 	{
 		type: "announcement",
+		id: "aws-marketplace",
+		breadCrumbTitle:
+			" Karini AI Now Available on AWS Marketplace!",
+		title: "Generative AI Goes Prime Time: Karini AI Now Available on AWS Marketplace!",
+		postedOn: "July 1, 2024",
+		timeToRead: "3 min read",
+		metadata:{
+				"og:local": "en_US",
+				"og:type": "article",
+				"og:title": "Karini AI Available on AWS - Secure GenAI Solutions",
+				"og:description":
+					"Launch your AI innovations with Karini AI on AWS Marketplace. Secure, scalable, and user-friendly!",
+				"og:url":
+					"https://www.karini.ai/announcements/aws-marketplace",
+				"og:site_name": "Karini AI on AWS MarketPlace",
+				"article:published_time": "2024-07-01",
+				"og:updated_time": "2024-07-01",
+				"og:image":
+					"https://www.karini.ai/_next/image?url=%2Fassets%2Fimages%2Fblogs%2FKarini_AI_now_on_AWS_Marketplace.png&w=640&q=75",
+				"og:image:secure_url":
+					"https://www.karini.ai/_next/image?url=%2Fassets%2Fimages%2Fblogs%2FKarini_AI_now_on_AWS_Marketplace.png&w=640&q=75",
+				"og:image:width": "640",
+				"og:image:height": "640",
+				"og:image:alt":
+					"Karini AI on AWS Marketplace",
+				"twitter:card": "summary",
+				"twitter:description":
+					"Unlock the power of GenAI with Karini AI on AWS Marketplace. Secure, scalable AI development is just a click away!",
+				"twitter:title":
+					"Karini AI Launches on AWS Marketplace - Explore Now!",
+				"twitter:site":
+					"https://www.karini.ai/announcements/aws-marketplace",
+				"twitter:image":
+					"https://www.karini.ai/_next/image?url=%2Fassets%2Fimages%2Fblogs%2FKarini_AI_now_on_AWS_Marketplace.png&w=640&q=75",
+				"twitter:creator": "Efficient Batch Pipelines with Karini AI's No-Code Generative AI Recipes",
+			},
+		SEO_data: {
+			title: "Karini AI Launches on AWS Marketplace - Explore Now!",
+			metaTitle: "Karini AI Available on AWS - Secure GenAI Solutions",
+			keywords:
+				"Karini AI AWS Marketplace, Generative AI AWS integration, AWS Marketplace GenAI solutions, Secure GenAI platform, No-code AI development AWS",
+			description:
+				"Discover Karini AI on AWS Marketplace! Build secure, scalable GenAI applications quickly with seamless integration and enhanced security.",
+			canonicalLink:
+				"https://www.karini.ai/announcements/aws-marketplace",
+			hreflang:
+				"https://www.karini.ai/announcements/aws-marketplace",
+			schemaMarkup: {
+				"@context": "https://schema.org",
+				"@type": "NewsArticle",
+				mainEntityOfPage: {
+					"@type": "WebPage",
+					"@id":
+						"https://www.karini.ai/announcements/aws-marketplace",
+				},
+				headline:
+					"Karini AI extends support for Batch execution pipelines using no-code Generative AI recipes",
+				image:
+					"https://www.karini.ai/assets/images/blogs/Karini_AI_now_on_AWS_Marketplace.png",
+				author: {
+					"@type": "Person",
+					name: "Deepali Rajale",
+				},
+				publisher: {
+					"@type": "Organization",
+					name: "Karini AI",
+					logo: {
+						"@type": "ImageObject",
+						url: "https://www.karini.ai",
+					},
+				},
+				datePublished: "2024-07-01",
+			},
+		},
+		sections: [
+			{
+				group: true,
+				data: [
+					{
+						type: type.paragraph,
+						fontSize: fontSize.medium,
+						fontWeight: fontWeight.normal,
+						fontStyle: fontStyle.normal,
+						value:
+						    "Empowering innovation while prioritizing security is paramount for businesses of all sizes. We're thrilled to announce that Karini AI, the leading platform for building secure and scalable Generative AI (GenAI) applications, is now available on AWS Marketplace. Its user-friendly interface and seamless integration with Amazon Bedrock make it a comfortable choice for your business."
+					},
+				],
+			},
+			{
+				group: true,
+				data: [
+					{
+						type: type.paragraph,
+						fontSize: fontSize.medium,
+						fontWeight: fontWeight.normal,
+						fontStyle: fontStyle.normal,
+						value: "Built upon the trusted foundation of AWS, Karini AI seamlessly integrates with Amazon Bedrock, empowering you to leverage state-of-the-art models with the peace of mind of enterprise-grade security. But Karini AI goes beyond just security. Our platform offers a suite of benefits that can unlock significant value for your business:"
+					},
+					{
+						type: type.list,
+						fontSize: fontSize.medium,
+						fontWeight: fontWeight.normal,
+						fontStyle: fontStyle.normal,
+						value: `<ul class="space-y-2" style="list-style-type: disc; padding-left: 50px">
+						        <li>
+								<h3 class="list-heading">Effortless Development & Deployment:</h3> Turn innovative ideas into reality in minutes, with no coding required. This frees your team to focus on creative and strategic tasks, leading to increased productivity.
+								</li>
+						        <li>
+								<h3 class="list-heading">Unparalleled Security & Governance:</h3>  Built-in observability and guardrails ensure responsible experimentation and deployment, while robust security features keep your data safe and compliant. This translates to enhanced security and maintained user trust, giving you a sense of confidence in your operations.
+								</li>
+								<li>
+								<h3 class="list-heading">Streamlined Workflow:</h3>  Integrate seamlessly with your existing AWS infrastructure for a frictionless experience.
+								</li>
+								<li>
+								<h3 class="list-heading">Faster Time to Market:</h3>  With Karini AI's rapid development tools, you can quickly bring your AI-powered products and services to market, giving you a crucial competitive edge and a sense of empowerment.
+								</li>
+								<li>
+								<h3 class="list-heading"> Improved Quality of AI Applications:</h3> Our trust, safety controls, and human feedback capabilities ensure reliable and high-quality AI outputs.
+								</li>
+								<li>
+								<h3 class="list-heading">Cost Savings:</h3>  Karini AI's detailed operational dashboards and cost management features help optimize your AI expenditures. The time savings you gain can translate to significant cost reductions, with projects potentially seeing 25-40% lower costs thanks to the 8-week development speedup.
+								</li>`
+					},
+					
+				],
+			},
+			{
+				group: true,
+				data: [
+					{
+						type: type.paragraph,
+						fontSize: fontSize.medium,
+						fontWeight: fontWeight.normal,
+						fontStyle: fontStyle.normal,
+						value:
+						    "The convenience of AWS Marketplace makes it easier than ever to unlock the power of GenAI. Now, you can easily procure and deploy Karini AI directly within your AWS environment, accelerating your journey to AI-driven innovation."
+					},
+				],
+			},
+			{
+				group: true,
+				data: [
+					{
+						type: type.paragraph,
+						fontSize: fontSize.medium,
+						fontWeight: fontWeight.normal,
+						fontStyle: fontStyle.normal,
+						value:
+							"Ready to unlock the transformative potential of GenAI? Don't wait, visit Karini AI on AWS Marketplace today and start your journey towards AI-driven innovation: <a style='text-decoration: underline; color:rgb(37 99 235 / var(--tw-text-opacity))' href='https://aws.amazon.com/marketplace/pp/prodview-7wz23ginqou4w?sr=0-1&ref_=beagle&applicationId=AWSMPContessa' target='_blank'>Link to Karini AI on AWS Marketplace</a>",
+					},
+				],
+			},
+			{
+				group: true,
+				data: [
+					{
+						type: type.paragraph,
+						fontSize: fontSize.medium,
+						fontWeight: fontWeight.normal,
+						fontStyle: fontStyle.normal,
+						value:
+						      "Don't let complexity hold you back. With Karini AI, embracing Generative AI is as easy as a few clicks. Our platform is designed for ease of use, allowing you to unlock various benefits that drive innovation and success without the need for extensive technical knowledge."
+					},
+				],
+			},
+		],
+	},
+	{
+		type: "announcement",
 		id: "batch-execution-pipelines",
 		breadCrumbTitle:
 			"Batch execution pipelines using no-code Generative AI recipes",
