87699138d2d0591daa3353896a457e8088d28cf4
Swapnil Kotkar <swapnil.kotkar@karini.ai>
2024-09-03 21:14:20 +0530

    updated

--- components/partners/databricks/Hero.jsx
+++ components/partners/databricks/Hero.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
@@ -13,10 +14,15 @@ const Hero = () => {
 						{/* <div className="absolute p-20 md:p-32 rounded-full left-0 md:left-1/4 top-10 md:top-20 bg-primary-100 blur-2xl"></div>
 			  <div className="hidden lg:block absolute p-20 md:p-32 rounded-full right-0 bottom-0 bg-primary-100 blur-2xl"></div> */}
 						<div className="z-10 space-y-3 px-4 max-w-screen-lg">
-							<h1 className="text-center text-2xl font-semibold text-white md:text-3xl lg:text-4xl">
-								Databricks
-								<span className="text-primary-600"> </span>
-							</h1>
+							<div className="flex justify-center">
+								<Image
+									src={"/assets/images/partners/databricks.png"}
+									alt="databricks"
+									width={400}
+									height={100}
+									className="bg-white p-4 rounded"
+								/>
+							</div>
 							<p className="text-center text-secondary-100 text-sm sm:text-base">
 								Redefining the future of data and AI solutions. The strategic
 								collaboration between Karini’s GenAI Foundation and Databricks
