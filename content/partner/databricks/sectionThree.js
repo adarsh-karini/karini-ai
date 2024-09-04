7a4898d4e32f6e89297b03ff393e6c4c005dee46
Nitin Wagh <nitinwagh@nitin-mbp.lan>
2024-09-04 10:40:34 -0400

    updated db page

--- content/partner/databricks/sectionThree.js
+++ content/partner/databricks/sectionThree.js
@@ -1,6 +1,6 @@
 export const sectionThree = [
 	{
-		title: "Set Up the GenAI Foundation",
+		title: "Set Up a Knowledgebase",
 		overview:
 			"Utilize Karini AI’s no-code recipes to create a data ingestion pipeline, preprocess data into LLM-ready formats, and store it in Databricks vector store. This ensures a scalable, cloud-agnostic GenAI platform ready for AI application development.",
 		videoPath: `<iframe width="560" height="315" src="https://www.youtube.com/embed/wkwxLmMTJYA?si=X4cSKRzcFFq_9IrA" title="Part 1: Creating a Knowlegebase" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
@@ -12,14 +12,14 @@ export const sectionThree = [
 		videoPath: `<iframe width="560" height="315" src="https://www.youtube.com/embed/553OaYvqtrA?si=oYUFoEPqO9n13GJV" title="Part 2: Creating a legal agent prompt" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
 	},
 	{
-		title: "Deploy and Monitor AI Applications",
+		title: "Deploy Agentic Chatbot",
 		overview:
 			"Deploy finalized prompts using a no-code recipe, configure chatbots with customization options, and use Karini AI’s cost and performance dashboards to track spending and optimize performance, ensuring continuous improvement and efficient operation.",
 		videoPath: `<iframe width="560" height="315" src="https://www.youtube.com/embed/87p11pmQh6I?si=uE75gRc8BeJRYaf3" title="Part 3: Building the legal recipe and copilot" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
 	},
 	{
-		title: "4th title",
-		overview: "4th_description",
+		title: "Review Prompt History",
+		overview: "Review conversation history and user feedback. This detailed history can be exported to a CSV file for offline analysis. Additionally, the enriched history and user feedback can be utilized to create RAG (Retrieval-Augmented Generation) ground truth datasets. These datasets are instrumental in improving RAG recipes or fine-tuning custom models, ensuring the continuous enhancement of AI applications.",
 		videoPath: `<iframe width="560" height="315" src="https://www.youtube.com/embed/AjHH88onE3g?si=WMgA8mAJ3spnsKzt&amp;controls=0" title="Part 4: Copilot History" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
 	},
 ];
